07cd7e718e732777820031d673c65b4750dbd5a9
JamieWalkerSTF <jamie@studiotwofold.com.au>
2023-12-07 16:16:05 +1000

    day 2 solution

--- src/routes/day-01/+page.server.ts
+++ src/routes/day-01/+page.server.ts
@@ -1,15 +1,23 @@
 import fs from 'fs';
 
-const data = fs.readFileSync('src/routes/day-02/data.txt', 'utf8').trim().split('\r\n');
-
-const maxValues = {
-	red: 12,
-	blue: 13,
-	green: 14
-};
+const data = fs.readFileSync('src/routes/day-01/example2.txt', 'utf8').trim().split('\r\n');
 
 //Calculate Part 1 Solution
-function getValue1(data: string[]) {}
+function getValue1(data: string[]) {
+	let output: number = 0;
+	data.map((item) => {
+		//find the first number in the string
+		const firstNumberObject = item.match(/\d+/);
+		const lastNumberObject = item.match(/\d+(?=\D*$)/);
+		//Validate the output incase there is a NULL/NAN
+		if (firstNumberObject !== null && lastNumberObject !== null) {
+			//console.log(firstNumberObject[0].charAt(0) + ',' + lastNumberObject[0].slice(-1));
+			const number = firstNumberObject[0].charAt(0) + lastNumberObject[0].slice(-1);
+			output += +number;
+		}
+	});
+	return output;
+}
 
 //Calculate Part 2 Solution
 // function getValue2(data: string[]) {
@@ -25,7 +33,7 @@ function getValue1(data: string[]) {}
 //Send function to frontend
 export function load() {
 	return {
-		part1: getValue1(data)
-		// part2: getValue2(data)
+		part1: getValue1(data),
+		part2: 'getValue2(data)'
 	};
 }
--- src/routes/day-02/+page.server.ts
+++ src/routes/day-02/+page.server.ts
@@ -1,29 +1,56 @@
 import fs from 'fs';
+const startTime = performance.now();
 
-const data = fs.readFileSync('src/routes/day-01/data.txt', 'utf8').trim().split('\r\n');
+const maxValues = { red: 12, blue: 14, green: 13 };
+
+// Read the data file and split into an array of strings
+const data = fs.readFileSync('src/routes/day-02/data.txt', 'utf8').trim().split('\r\n');
+//Clean the data, remove "Game `: " from the string
+const cleanData: string[] = data.map((game) => {
+	const splitString = game.split(': ')[1].trim();
+	return splitString;
+});
+
+//console.log(cleanData);
+const gameResultArray: boolean[] = [];
 
 //Calculate Part 1 Solution
-function getValue1(data: string[]) {
-	const gameData = data.map((item) => {
-		console.log(item);
+const getValue1 = cleanData.map((game) => {
+	const pairs = game.split(/,|;/).map((pair) => pair.trim()); // [ '3 blue', '4 red', '1 red', '2 green', '6 blue', '2 green' ]
+	//console.log('GAME:');
+
+	let gameResult = true;
+
+	pairs.forEach((pair) => {
+		const [number, color] = pair.split(' ').map((item) => item.trim()); // [ '3', 'blue' ]
+		// Convert string to number
+		const num = parseInt(number, 10);
+
+		if (maxValues[color] !== undefined && num > maxValues[color]) {
+			//.log('FALSE');
+			gameResult = false;
+			return; // Number exceeds the max value
+		}
 	});
-}
 
-//Calculate Part 2 Solution
-// function getValue2(data: string[]) {
+	gameResultArray.push(gameResult);
+	// console.table(splitString);
+});
 
-// 	data.map((item) => {
-// 		//find the first number in the string
-//     let text = "Hello world, welcome to the universe.";
-//     let result = text.includes("world");
-
-//   });
-// };
+let totalPossible = 0;
+gameResultArray.forEach((result, index) => {
+	if (result) {
+		totalPossible += index + 1;
+	}
+});
 
 //Send function to frontend
 export function load() {
 	return {
-		part1: 'Not yet implemented'
+		part1: totalPossible
 		// part2: getValue2(data)
 	};
 }
+
+const endTime = performance.now();
+console.log(`${endTime - startTime} milliseconds`);
